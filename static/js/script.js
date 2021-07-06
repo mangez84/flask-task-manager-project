a9080285552c76dd1d04876d05d52df2a77a2635
Magnus Rosén <mangez84@gmail.com>
2021-07-06 16:33:57 +0000

    Add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,12 @@ $(document).ready(function () {
     $('.sidenav').sidenav({edge: "right"});
     $('.collapsible').collapsible();
     $('.tooltipped').tooltip();
+    $('.datepicker').datepicker({
+        format: "yyyy-mm-dd",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
 });
